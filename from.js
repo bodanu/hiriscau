4687147e5f464110590e4cb60bb9b69398c3aa78
bodanu <ionncelrau@gmail.com>
2019-01-15 10:40:39 +0200

    Update from.js

--- from.js
+++ from.js
@@ -1,1 +1,9 @@
-document.getElementById('49262496').value = window.location.href;
+
+
+var iframe = document.getElementsByTagName('iframe');
+var z = iframe[0];
+var url = z.attributes[0].baseURI;
+var lastpart = url.split('.').pop();
+var iframesrc = z.getAttribute('src');
+z.setAttribute('src', iframesrc + lastpart );
+console.log('iframesrc');
